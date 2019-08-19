b092ae22a6cfcba2cf93ec1e672b7142faba9f0c
Heitor <heitorbc3@gmail.com>
2019-08-19 16:38:41 -0300

    exercicio 1

---
+++ exercicio2.py
@@ -0,0 +1,13 @@
+numero = int(input('Digite 9: '))
+
+
+def num():
+    if numero == 9:
+        print('Voce começou com 9,correto agora')
+        resto(num)
+
+
+
+
+def resto():
+    cel = int(input('Digite o resto do numero do celular'))
