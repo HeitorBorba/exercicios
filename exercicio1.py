87165229a4ffce587a83bbd6d0382a47f4ad0fbc
Heitor <heitorbc3@gmail.com>
2019-08-19 16:55:01 -0300

    atualizar

--- exercicio1.py
+++ exercicio1.py
@@ -1,7 +1,6 @@
-numero = float(input('Digite um número para saber se é par ou impar: '))
-resto = numero % 2
-
-if resto == 0:
-    print('Número é par')
-else:
-    print('Número é impar')
+listas = [0,1,2,3,4,5,6,7,8,9,10]
+for lista in listas:
+    if lista % 2 == 0:
+        print('Número é '  + str(lista) +  ' par')
+    else:
+        print('Número é '  + str(lista) +  ' impar')
--- exercicio2.py
+++ exercicio2.py
@@ -4,10 +4,9 @@ numero = int(input('Digite 9: '))
 def num():
     if numero == 9:
         print('Voce começou com 9,correto agora')
-        resto(num)
+
+    else:
+        print('voce digitou por favor digite 9'
 
 
 
-
-def resto():
-    cel = int(input('Digite o resto do numero do celular'))
